86ae5b0e9388e0b485a08c268622817d71d01689
yamamoto2 <f.yuu1128.yuu1128@gmail.com>
2025-09-02 14:27:34 +0900

    メール機能の実装

---
+++ laravel-app/html/resources/views/contact-email.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>お問い合わせ返信メール</title>
+    <link rel="stylesheet" href="{{ asset('css/email/notice.css') }}" />
+</head>
+
+<body>
+    <p>{!! nl2br(e($main)) !!}</p>
+</body>
+
+</html>
